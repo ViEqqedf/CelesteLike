715e3ff27937298439ec04dcd8e526d21c263cd3
VirrorE <vieqqedf@gmail.com>
2025-02-06 18:13:19 +0800

    <misc> basic horizontal move

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,131 +3,129 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class InputManager : MonoBehaviour {
-
-    public static InputManager Instance;
-    // PlayerCharacter character;
+namespace ViE.CelesteLike {
+    public class InputManager : MonoBehaviour {
+        public static InputManager Instance;
 
     #region Key
-    [Header("控制是否使用自定义按键")]
-    public bool IsKeySet;
-    [Header("左移动")]
-    public KeyCode LeftMoveKey;
-    [Header("右移动")]
-    public KeyCode RightMoveKey;
-    [Header("跳跃按键")]
-    public KeyCode Jump;
-    [Header("冲刺按键")]
-    public KeyCode Dash;
-    [Header("爬墙按键")]
-    public KeyCode Climb;
-    [HideInInspector]
-    public bool ClimbKey => Input.GetKey(Climb);
-    [HideInInspector]
-    public bool ClimbKeyDown => Input.GetKeyDown(Climb);
-    [HideInInspector]
-    public bool ClimbKeyUp => Input.GetKeyUp(Climb);
-    [HideInInspector]
-    public bool JumpKey => Input.GetKey(Jump);
-    [HideInInspector]
-    public bool JumpKeyDown {
-        get {
-            if(Input.GetKeyDown(Jump)) {
-                return true;
-            } else if(JumpFrame > 0) {
-                return true;
+        public bool UseCustomKey;
+        public KeyCode UpMoveKey;
+        public KeyCode DownMoveKey;
+        public KeyCode LeftMoveKey;
+        public KeyCode RightMoveKey;
+        public KeyCode Jump;
+        public KeyCode Dash;
+        public KeyCode Climb;
+        public bool ClimbKey => Input.GetKey(Climb);
+        public bool ClimbKeyDown => Input.GetKeyDown(Climb);
+        public bool ClimbKeyUp => Input.GetKeyUp(Climb);
+        public bool JumpKey => Input.GetKey(Jump);
+        public bool JumpKeyDown {
+            get {
+                if (Input.GetKeyDown(Jump)) {
+                    return true;
+                } else if (JumpFrame > 0) {
+                    return true;
+                }
+                return false;
             }
-            return false;
         }
-    }
-    [HideInInspector]
-    public bool JumpKeyUp => Input.GetKeyUp(Jump);
-    [HideInInspector]
-    public bool DashKey => Input.GetKey(Dash);
-    [HideInInspector]
-    public bool DashKeyDown => Input.GetKeyDown(Dash);
-    [HideInInspector]
-    public bool DashKeyUp => Input.GetKeyUp(Dash);
+        public bool JumpKeyUp => Input.GetKeyUp(Jump);
+        public bool DashKey => Input.GetKey(Dash);
+        public bool DashKeyDown => Input.GetKeyDown(Dash);
+        public bool DashKeyUp => Input.GetKeyUp(Dash);
     #endregion
 
-    [HideInInspector]
-    public float v = 0;
-    public float h = 0;
-    public AnimationCurve MoveStartCurve;
-    public AnimationCurve MoveEndCurve;
-    [SerializeField]
-    float MoveStartTime;
-    [SerializeField]
-    float MoveEndTime;
-    [SerializeField]
-    public int MoveDir;
+        public float v = 0;
+        public float h = 0;
+        public AnimationCurve MoveStartCurve;
+        public AnimationCurve MoveEndCurve;
+        [SerializeField]
+        public int MoveDir;
+        [SerializeField]
+        private float MoveStartTime;
+        [SerializeField]
+        private float MoveEndTime;
 
-    private int JumpFrame;
-    private void Awake() {
-        if (Instance != null) {
-            Destroy(gameObject);
-            return;
-        }
+        private int JumpFrame;
 
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-        // character = GetComponent<PlayerCharacter>();
-        KeyInit();
-    }
-    public void KeyInit() {
-        if (!IsKeySet) {
-            Jump = KeyCode.K;
-            Dash = KeyCode.J;
-            Climb = KeyCode.L;
-            LeftMoveKey = KeyCode.A;
-            RightMoveKey = KeyCode.D;
-        }
-    }
-
-    private void FixedUpdate() {
-        if(JumpFrame >= 0) {
-            JumpFrame--;
-        }
-    }
-
-    private void Update() {
-        CheckHorizontalMove();
-
-        bool isRightMoveKeyDown = Input.GetKey(RightMoveKey);
-        bool isLeftMoveKeyDown = Input.GetKey(LeftMoveKey);
-        if (isRightMoveKeyDown && isLeftMoveKeyDown || !isRightMoveKeyDown && !isLeftMoveKeyDown) {
-            h = 0;
-        } else if (isRightMoveKeyDown) {
-            h = 1;
-        } else if (isLeftMoveKeyDown) {
-            h = -1;
-        }
-
-        // v = Input.GetAxisRaw("Vertical");
-        // h = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetKeyDown(Jump)) {
-            JumpFrame = 3;
-        }
-    }
-
-    void CheckHorizontalMove() {
-        if (Input.GetKeyDown(RightMoveKey) && h <= 0) {
-            MoveDir = 1;
-        } else if (Input.GetKeyDown(LeftMoveKey) && h >= 0) {
-            MoveDir = -1;
-        } else if (Input.GetKeyUp(RightMoveKey)) {
-            if (Input.GetKey(LeftMoveKey)) {
-                MoveDir = -1;
-                MoveStartTime = Time.time;
-            } else {
-                MoveDir = 0;
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
             }
-        } else if (Input.GetKeyUp(LeftMoveKey)) {
-            if (Input.GetKey(RightMoveKey)) {
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            KeyInit();
+        }
+        public void KeyInit() {
+            if (!UseCustomKey) {
+                Jump = KeyCode.K;
+                Dash = KeyCode.J;
+                Climb = KeyCode.L;
+                UpMoveKey = KeyCode.W;
+                DownMoveKey = KeyCode.S;
+                LeftMoveKey = KeyCode.A;
+                RightMoveKey = KeyCode.D;
+            }
+        }
+
+        private void FixedUpdate() {
+            if (JumpFrame >= 0) {
+                JumpFrame--;
+            }
+        }
+
+        private void Update() {
+            CheckHorizontalMove();
+
+            bool isRightMoveKeyDown = Input.GetKey(RightMoveKey);
+            bool isLeftMoveKeyDown = Input.GetKey(LeftMoveKey);
+            if (isRightMoveKeyDown && isLeftMoveKeyDown ||
+                !isRightMoveKeyDown && !isLeftMoveKeyDown) {
+                h = 0;
+            } else if (isRightMoveKeyDown) {
+                h = 1;
+            } else if (isLeftMoveKeyDown) {
+                h = -1;
+            }
+
+            // v = Input.GetAxisRaw("Vertical");
+            bool isUpMoveKeyDown = Input.GetKey(UpMoveKey);
+            bool isDownMoveKeyDown = Input.GetKey(DownMoveKey);
+            if (isUpMoveKeyDown && isDownMoveKeyDown ||
+                !isUpMoveKeyDown && !isDownMoveKeyDown) {
+                v = 0;
+            } else if (isUpMoveKeyDown) {
+                v = 1;
+            } else if (isDownMoveKeyDown) {
+                v = -1;
+            }
+
+            if (Input.GetKeyDown(Jump)) {
+                JumpFrame = 3;
+            }
+        }
+
+        void CheckHorizontalMove() {
+            if (Input.GetKeyDown(RightMoveKey) && h <= 0) {
                 MoveDir = 1;
-            } else {
-                MoveDir = 0;
+            } else if (Input.GetKeyDown(LeftMoveKey) && h >= 0) {
+                MoveDir = -1;
+            } else if (Input.GetKeyUp(RightMoveKey)) {
+                if (Input.GetKey(LeftMoveKey)) {
+                    MoveDir = -1;
+                    MoveStartTime = Time.time;
+                } else {
+                    MoveDir = 0;
+                }
+            } else if (Input.GetKeyUp(LeftMoveKey)) {
+                if (Input.GetKey(RightMoveKey)) {
+                    MoveDir = 1;
+                } else {
+                    MoveDir = 0;
+                }
             }
         }
     }
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using UnityEngine;
+
+namespace ViE.CelesteLike {
+    public enum PlayState {
+        Normal,
+        Jump,
+        Climb,
+        Dash,
+        Fall,
+    }
+
+    public class PlayerCharacter : MonoBehaviour {
+        public float MoveSpeed = 9;
+        public Vector3 Velocity;
+        public Vector3 CenterOffset;
+
+        private Rigidbody2D rig;
+        private int playerLayerMask;
+        private RaycastHit2D UpBox;
+        private RaycastHit2D DownBox;
+        private RaycastHit2D LeftBox;
+        private RaycastHit2D RightBox;
+        private Vector2 Position => transform.position - CenterOffset;
+        private bool isOnGround => DownBox.collider != null;
+        private Vector2 boxSize;
+
+        private void Start() {
+            rig = GetComponent<Rigidbody2D>();
+            playerLayerMask = LayerMask.GetMask("Player");
+            playerLayerMask = ~playerLayerMask;
+            boxSize = new Vector2(1, 1.2f);
+        }
+
+        private void FixedUpdate() {
+            HorizontalMove();
+            rig.MovePosition(transform.position + Velocity * Time.fixedDeltaTime);
+        }
+
+        private void Update() {
+            RayCastBox();
+        }
+
+        private void HorizontalMove() {
+            var input = InputManager.Instance;
+            if ((Velocity.x > 0 && input.MoveDir == -1) ||
+                (Velocity.x < 0 && input.MoveDir == 1) ||
+                input.MoveDir == 0 ||
+                (isOnGround && input.v < 0) ||
+                Mathf.Abs(Velocity.x) > MoveSpeed) {
+                // decelerate section
+                int hvDir = Velocity.x > 0 ? 1 : -1;
+                float hMove = Mathf.Abs(Velocity.x);
+                if (isOnGround) {
+                    hMove -= MoveSpeed / 3;
+                } else {
+                    hMove -= MoveSpeed / 6;
+                }
+
+                if (hMove < 0.01f) {
+                    hMove = 0;
+                }
+
+                Velocity.x = hMove * hvDir;
+            } else {
+                // accelerate section
+                if (isOnGround && input.v < 0) {
+                    return;
+                }
+
+                if (input.MoveDir == 1) {
+                    if (isOnGround) {
+                        Velocity.x += MoveSpeed / 6;
+                    } else {
+                        Velocity.x += MoveSpeed / 12;
+                    }
+
+                    if (Velocity.x > MoveSpeed) {
+                        Velocity.x = MoveSpeed;
+                    }
+                } else if (input.MoveDir == -1 && !(isOnGround && input.v < 0)) {
+                    if (isOnGround) {
+                        Velocity.x -= MoveSpeed / 6;
+                    } else {
+                        Velocity.x -= MoveSpeed / 12f;
+                    }
+
+                    if (Velocity.x < -MoveSpeed) {
+                        Velocity.x = -MoveSpeed;
+                    }
+                }
+            }
+        }
+
+        private void Fall() {
+            if (isOnGround) {
+                Velocity.y = 0;
+                return;
+            }
+
+            Velocity.y -= 150f * Time.deltaTime;
+            Velocity.y = Mathf.Clamp(Velocity.y, -25, Velocity.y);
+        }
+
+        private void RayCastBox() {
+            UpBox = Physics2D.BoxCast(Position, boxSize, 0, Vector3.up, 0.1f, playerLayerMask);
+            DownBox = Physics2D.BoxCast(Position, boxSize, 0, Vector3.down, 0.1f, playerLayerMask);
+            LeftBox = Physics2D.BoxCast(Position, boxSize, 0, Vector3.left, 0.1f, playerLayerMask);
+            RightBox = Physics2D.BoxCast(Position, boxSize, 0, Vector3.right, 0.1f, playerLayerMask);
+        }
+    }
+}
